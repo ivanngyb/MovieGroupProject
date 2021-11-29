4dac61dd62db4c4bcf6590fd4c6bb4370d114e73
Caspian Maclean <30039802@tafe.wa.edu.au>
2021-11-29 10:57:19 +0800

    Fix star update - update order for MySQL

--- web_files/add_rating_script.php
+++ web_files/add_rating_script.php
@@ -16,13 +16,19 @@ if (! array_key_exists('stars', $_GET) || !array_key_exists('id', $_GET)) {
     $id = $_GET['id'];
     $stars_to_add = $_GET['stars'];
 
+
+    // For MySQL, the field that depends on the old value of
+    // other fields must be listed first: average_star_rating
+    // Other SQL databases provide the old value of fields for all the expressions,
+    // which is the clearest interpretation of the SQL standard, 
+    // See https://dev.mysql.com/doc/refman/8.0/en/ansi-diff-update.html
     $stmt = $conn->prepare(
         '
     UPDATE `dvd`
     SET
+        average_star_rating = (total_stars + :stars_to_add) / (number_of_star_ratings + 1),
         number_of_star_ratings = number_of_star_ratings + 1,
-        total_stars = total_stars + :stars_to_add,
-        average_star_rating = (total_stars + :stars_to_add) / (number_of_star_ratings + 1)
+        total_stars = total_stars + :stars_to_add
     WHERE
         id=:id
     ;
